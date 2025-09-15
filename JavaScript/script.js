922e7ba10faa78274401e54f2116fe6701814854
Ziyavudeen <ziyavudeen2004@gmail.com>
2025-09-15 22:44:10 +0530

    Update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,3 +8,20 @@ function resume(){
 };
 // let hireMe=document.getElementById('but1');
 // hireMe.addEventListener('click',)
+
+
+
+// let a=document.getElementById("form");
+// a.addEventListener("submit",fromContact);
+
+// fromContact(){
+
+// }
+
+// emailjs.send("service_32or4cj","template_43pdmhh",this,"8QcjdBao9kzL3QL7D")
+// .then(()=>{
+//     alert("Succesfull");
+// })
+// .catch(()=>{
+//     error("Not succesfull");
+// })
